54aba06f32518660240b516649bf79ff70954253
David Nieto Garcia <david.nietog@uanl.edu.mx>
2025-04-09 16:53:23 -0600

    clase

---
+++ DavidNGpy/clases.py
@@ -0,0 +1,15 @@
+class MiClase:
+    def __init__(self,num1,num2,num3):
+        self.num1=num1
+        self.num2=num2
+        self.num3=num3
+    def Sumar(self):
+       return sum(self.num1+self.num2+self.num3)
+    def Mayor(self):
+       return max(self.num1,self.num2,self.num3)
+    def Menor(self):
+       return min(self.num1,self.num2,self.num3)
+    def Iguales(self):
+      pass
+    def Concatenar(self):
+      pass
