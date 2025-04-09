8baa8ed092b1cba34c82cae93ba392a543878c65
Bryan Said Chavez Morales <bryan.chavez5806@gmail.com>
2025-04-09 17:35:46 -0600

    agregue metodos mayor, menor, iguales y concatenar

--- bryanchvz/clase.py
+++ bryanchvz/clase.py
@@ -7,3 +7,32 @@ class Mi_clase:
     def sumar(self):
         resultado = self.num1 + self.num2 + self.num3
         return resultado
+    
+    def mayor(self):
+        resultado = max(self.num1,self.num2,self.num3)
+        return resultado
+    
+    def menor(self):
+        resultado = min(self.num1,self.num2,self.num3)
+        return resultado
+    
+    def iguales(self):
+        if self.num1 == self.num2 and self.num2 == self.num3:
+            resultado = True
+        else:
+            resultado = False
+        return resultado
+    
+    def concatenar(self):
+        concatenado = str(self.num1) + str(self.num2) + str(self.num3)
+        return concatenado
+
+num1 = int(input("Ingresa un primer numero: "))
+num2 = int(input("Ingresa un segundo numero: "))
+num3 = int(input("Ingresa un tercer numero: "))
+num_nuevo = Mi_clase(num1,num2,num3)
+
+print("La suma es: ", num_nuevo.sumar())
+print("El numero mayor es: ",num_nuevo.mayor())
+print("Los numeros son iguales? true/false: ", num_nuevo.iguales())
+print("Numeros concatenados: ", num_nuevo.concatenar())
