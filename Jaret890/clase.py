295b107e7f35e902e369118cc23e4852369581ce
Edson Jaret Gomez Patiño <carlo_r12@hotmail.com>
2025-04-09 20:25:54 -0600

    Se probaron los metodos y se hicieron algunos cambios en ellos porque mrcaba error

--- Jaret890/clase.py
+++ Jaret890/clase.py
@@ -5,26 +5,24 @@ class Mi_clase:
         self.num3 = num3
     
     def suma(self):
-        return sum(self.num1,self.num2,self.num3)
+        return self.num1 + self.num2 + self.num3
     
     def mayor(self):
-        if self.num1 > self.num2 and self.num1 > self.num3:
-            print(self.num1)
-        elif self.num2 > self.num1 and self.num2 > self.num3:
-            print(self.num2)
-        elif self.num3 > self.num1 and self.num3 > self.num2:
-            print(self.num3)
+        return max(self.num1,self.num2,self.num3)
 
     def menor(self):
-        if self.num1 < self.num2 and self.num1 < self.num3:
-            print(self.num1)
-        elif self.num2 < self.num1 and self.num2 < self.num3:
-            print(self.num2)
-        elif self.num3 < self.num1 and self.num3 < self.num2:
-            print(self.num3)
-
+        return min(self.num1,self.num2,self.num3)
     def iguales(self):
         return self.num1 == self.num2 == self.num3
 
     def concatenar(self):
         return f"{self.num1}{self.num2}{self.num3}"
+    
+
+num = Mi_clase(3,3,3)
+
+print("Suma de los numeros", num.suma())
+print("El mayor", num.mayor())
+print("El menor", num.menor())
+print("Son iguales?", num.iguales())
+print("Concatenados", num.concatenar())
