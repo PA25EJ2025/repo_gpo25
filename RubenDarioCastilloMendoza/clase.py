2ce1412126ad6850e9677f4d5d369e6bb17e099c
Ruben Dario Castillo Mendoza <dariomalodeleon@gmail.com>
2025-04-10 18:30:59 -0600

    codigo finalizado

--- RubenDarioCastilloMendoza/clase.py
+++ RubenDarioCastilloMendoza/clase.py
@@ -14,13 +14,23 @@ class Numeros:
     def menor(self):
         return min(self.numero1,self.numero2,self.numero3)
     
-    def Iguales(self):
+    def iguales(self):
         if self.numero1 == self.numero2 and self.numero2 == self.numero3:
             return True
         else:
             return False
         
-    def Concatenar(self):
-        return str(self.num1) + str(self.num2) + str(self.num3)
+    def concatenar(self):
+        return str(self.numero1) + str(self.numero2) + str(self.numero3)
     
-    
+numero1=int(input("Ingresa un numero"))
+numero2=int(input("Ingresa un numero"))
+numero3=int(input("Ingresa un numero"))
+
+conjunto = Numeros(numero1,numero2,numero3)
+
+print(f"Resultado de suma :",conjunto.sumar())
+print(f"Resultado de mayor :",conjunto.mayor())
+print(f"Resultado de menor :",conjunto.menor())
+print(f"Resultado de iguales ",conjunto.iguales())
+print(f"Resultado de concatenar :",conjunto.concatenar())
