f5c09fa26c4ccf5019e471e6c7c25c03320f1587
Ruben Dario Castillo Mendoza <dariomalodeleon@gmail.com>
2025-04-10 18:13:15 -0600

    Metodo sumar creado

--- RubenDarioCastilloMendoza/clase.py
+++ RubenDarioCastilloMendoza/clase.py
@@ -1,6 +1,9 @@
 class Numeros:
-    
+
     def __init__(self,numero1,numero2,numero3):
         self.numero1 = numero1
         self.numero2 = numero2
         self.numero3 = numero3
+
+    def sumar(self):
+        return self.numero1 + self.numero2 + self.numero3
