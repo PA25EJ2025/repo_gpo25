8e113b4bf94c4abc6ec7af8fe7ef213c61e2110d
Angel Alvarez <jose.alvareztnc@uanl.edu.mx>
2025-04-11 22:39:27 -0600

    Funciones necesarias creadas con exito

---
+++ AngelAlvarez19/clases.py
@@ -0,0 +1,19 @@
+class Numeros:
+    def __init__ (self, num1, num2, num3):
+        self.num1 = num1
+        self.num2 = num2
+        self.num3 = num3
+    
+    def SUMA(self):
+        Suma = (self.num1) + (self.num2) + (self.num3)
+        return Suma
+    
+    def MAYOR(self):
+        return max(self.num1, self.num2, self.num3)
+    
+    def MENOR(self):
+        return min(self.num1, self.num2, self.num3)
+    
+    def CONCATENAR(self):
+        Conca = (str(self.num1)) + (str(self.num2)) + (str(self.num3))
+        return Conca
