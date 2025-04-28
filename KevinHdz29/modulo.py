3c91cc5755bd1d1515d6bc5af0f0a5dc39d857b8
Kevin Eduardo Hernandez Lopez <kevin.edy.029@gmail.com>
2025-04-28 17:41:01 -0600

    codigo de modulo

--- KevinHdz29/modulo.py
+++ KevinHdz29/modulo.py
@@ -1,2 +1,22 @@
+import pygame
 class Perfil:
-    pass
+    def __init__(self,x,y,ancho,alto,velocidad):
+        self.x = x
+        self.y = y
+        self.ancho = ancho
+        self.alto = alto
+        self.imagen = pygame.image.load("foto.png")
+        self.velocidad = velocidad
+    def mostrar(self,ventana):
+        ventana.blit(self.imagen,(self.x,self.y,self.ancho,self.alto))
+    def mover(self,x,y):
+        self.x = x
+        self.y = y
+    def arriba(self):
+        self.y = self.y - self.velocidad
+    def abajo(self):
+        self.y = self.y + self.velocidad
+    def izquierda(self):
+        self.x = self.x - self.velocidad
+    def derecha(self):
+        self.x = self.x + self.velocidad
