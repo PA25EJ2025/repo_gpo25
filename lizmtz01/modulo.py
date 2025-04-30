f8ad37ea4cbc42371060e60bbcecda4f6e8e47df
lizmtz01 <vlizethm00@hotmail.com>
2025-04-29 22:36:24 -0600

    codigo clase y main

--- lizmtz01/modulo.py
+++ lizmtz01/modulo.py
@@ -1,2 +1,13 @@
+import pygame
+
 class Perfil:
-    pass
+
+    def __init__(self, x, y, alto, ancho, ruta_imagen, velocidad):
+        self.imagen = pygame.image.load(ruta_imagen)
+        self.imagen = pygame.transform.scale(self.imagen, (ancho, alto))
+        self.rect = self.imagen.get_rect()
+        self.rect.topleft = (x, y)
+        self.velocidad = velocidad
+
+    def mostrar(self, ventana):
+        ventana.blit(self.imagen, self.rect)
